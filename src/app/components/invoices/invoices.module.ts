d2574b6e1e9c587ea7800bb4c0c96b405eccd770
Roto1707 <hieunguyen17071997@gmail.com>
2023-08-22 20:43:57 +0700

    add paypal

--- src/app/components/invoices/invoices.module.ts
+++ src/app/components/invoices/invoices.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InvoicesRoutingModule } from './invoices-routing.module';
+import { PaypalModule } from './paypal/paypal.module';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		InvoicesRoutingModule
+		InvoicesRoutingModule,
+		PaypalModule
 	]
 })
 export class InvoicesModule { }
